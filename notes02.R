446c9fa769629a2956eeda31eea4973dabe5fc92
Ryne VanKrevelen <rvankrevelen@elon.edu>
2024-02-09 09:59:42 -0500

    Added final version of notes 2

--- notes02.R
+++ notes02.R
@@ -19,6 +19,9 @@ sd(mtcars$disp)
 quantile(mtcars$wt, probs = c(0.2, 0.8))
 
 
+# Load dplyr package to use summarize() function
+# Calculate multiple statistics that will be outputted as a data frame
+
 library(dplyr)
 summarize(mtcars,
           min_mpg = min(mpg),
@@ -38,11 +41,56 @@ car_sum$avg_hp
 
 
 
+# Load moderndive package to access MA_schools data frame
+# Use str() to see the structure of MA_schools
+
 library(moderndive)
 str(MA_schools)
 
 
-# School size tables
+# School size tables (counts and then proportions)
 
 table(MA_schools$size)
 prop.table(table(MA_schools$size))
+
+
+
+# Summaries by groups (use .by argument in summarize())
+
+summarize(MA_schools,
+          count = n(),
+          .by = size)
+
+
+# Practice: Update earlier code to get separate summary for each transmission type
+
+car_sum <- summarize(mtcars,
+                     min_mpg = min(mpg),
+                     max_mpg = max(mpg),
+                     avg_hp = mean(hp),
+                     sd_hp = sd(hp),
+                     .by = am)
+car_sum
+
+
+
+# Practice: Group previous output by am and cyl
+
+car_sum <- summarize(mtcars,
+                     min_mpg = min(mpg),
+                     max_mpg = max(mpg),
+                     avg_hp = mean(hp),
+                     sd_hp = sd(hp),
+                     .by = c(am, cyl))
+car_sum
+
+# Bonus: Add count variable too
+
+car_sum <- summarize(mtcars,
+                     count = n(),
+                     min_mpg = min(mpg),
+                     max_mpg = max(mpg),
+                     avg_hp = mean(hp),
+                     sd_hp = sd(hp),
+                     .by = c(am, cyl))
+car_sum
