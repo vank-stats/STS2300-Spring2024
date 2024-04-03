670df0b2ddb61b179be934cb3d170e7214dba696
Ryne VanKrevelen <rvankrevelen@elon.edu>
2024-04-03 13:41:37 -0400

    Added notes 9 code

--- notes09.R
+++ notes09.R
@@ -34,6 +34,10 @@ HOR_boot <- mysamp %>%
   generate(reps = 1000, type = "bootstrap") %>%
   calculate(stat = "prop")
 
+
+# I added our sample proportion to the bootstrap distribution as a reminder
+#  that bootstrap distributions are centered on a statistic of interest
+
 visualize(HOR_boot) +
   geom_vline(xintercept = 14 / 30,
              color = "green",
@@ -46,9 +50,42 @@ visualize(HOR_boot) +
 phat <- mean(mysamp$party == "Democratic")
 phat
 
+
 # Calculate a theory-based CI using our formula
 # You will not need to calculate CIs in this way. We have functions for this.
 
 ci_theory <- c(phat - 1.645 * sqrt(phat * (1 - phat) / 30),
                phat + 1.645 * sqrt(phat * (1 - phat) / 30))
 ci_theory
+
+
+# Calculate a standard error method CI using the formula
+# You will not need to calculate CIs in this way. We have functions for this.
+
+c(phat - 1.645 * sd(HOR_boot$stat), 
+  phat + 1.645 * sd(HOR_boot$stat))
+
+# Calculating SE method CI using get_ci()
+
+ci_se <- HOR_boot %>%
+  get_ci(level = 0.9, type = "se", point_estimate = phat)
+ci_se
+
+
+# Calculating percentile method CI using get_ci()
+
+ci_perc <- HOR_boot %>%
+  get_ci(level = 0.9, type = "percentile")
+ci_perc
+
+
+# Displaying CIs on bootstrap distribution graphs
+
+library(patchwork)
+
+se <- visualize(HOR_boot) +
+  shade_ci(ci_se)
+perc <- visualize(HOR_boot) +
+  shade_ci(ci_perc, fill = "violet", color = "lightpink")
+
+se + perc
