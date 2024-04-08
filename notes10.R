46b066787d6f723501b0165628d68ef48f9f5c0e
Ryne VanKrevelen <rvankrevelen@elon.edu>
2024-04-08 10:34:37 -0400

    Added notes 10 code

--- notes10.R
+++ notes10.R
@@ -8,6 +8,9 @@ library(ggplot2)
 library(palmerpenguins)
 
 
+# Part 1 -- Confidence intervals for p
+
+
 # Read in data
 
 house_of_reps <- read.csv("https://raw.githubusercontent.com/vank-stats/STS2300-Spring2024/main/data/house_of_reps.csv")
@@ -59,3 +62,48 @@ prop.test(x = 14,
 prop.test(x = 14,
           n = 30,
           conf.level = 0.9)$conf.int
+
+
+
+# Part 2 -- Confidence intervals for mu
+
+
+# Calculate a sample mean
+
+bill_xbar <- mean(penguins$bill_length_mm, na.rm = TRUE)
+bill_xbar
+
+
+# Generate bootstrap distribution of sample means and graph it 
+
+set.seed(2448945)
+bill_boot <- penguins %>%
+  specify(formula = bill_length_mm ~ NULL) %>%
+  generate(reps = 1000, type = "bootstrap") %>%
+  calculate(stat = "mean")
+
+visualize(bill_boot) +
+  geom_vline(xintercept = bill_xbar,
+             color = "lightblue",
+             linewidth = 3)
+
+
+# 95% CI (SE method and percentile method)
+
+get_ci(bill_boot,
+       level = 0.95,
+       type = "se",
+       point_estimate = bill_xbar)
+
+bill_boot %>%
+  get_ci(level = 0.95,
+         type = "percentile")
+
+
+# 95% Theory-Based Confidence Interval
+
+t.test(x = penguins$bill_length_mm, 
+       conf.level = 0.95)
+
+t.test(x = penguins$bill_length_mm, 
+       conf.level = 0.95)$conf.int
